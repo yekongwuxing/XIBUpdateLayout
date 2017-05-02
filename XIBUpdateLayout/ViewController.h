5f12ba3848a5b5f4ac2b96b1daa2568596c2a737
yekongwuxing <yekongwuxing>
2017-05-02 11:23:32 +0800

    Initial Commit

---
+++ XIBUpdateLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XIBUpdateLayout
+//
+//  Created by jiangmm on 17/5/2.
+//  Copyright © 2017年 jchvip.rch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XIBUpdateLayoutTests/XIBUpdateLayoutTests.m
@@ -0,0 +1,39 @@
+//
+//  XIBUpdateLayoutTests.m
+//  XIBUpdateLayoutTests
+//
+//  Created by jiangmm on 17/5/2.
+//  Copyright © 2017年 jchvip.rch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XIBUpdateLayoutTests : XCTestCase
+
+@end
+
+@implementation XIBUpdateLayoutTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
